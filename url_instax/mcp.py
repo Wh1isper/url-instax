0eb677382a9fd412b398faf1d3aef81752946fe1
Wh1isper <9573586@qq.com>
2025-04-20 16:33:56 +0800

    Implement screenshot http

--- url_instax/mcp.py
+++ url_instax/mcp.py
@@ -6,6 +6,6 @@ from url_instax.config import get_config
 app = FastMCP("url_instax")
 config = get_config()
 client = AsyncClient(
-    base_url=config.api_base_url,
+    base_url=config.api_base_url or "",
     headers={"Authorization": f"Bearer {config.api_token}"} if config.api_token else {},
 )
--- url_instax/routers/api/v1/screenshot.py
+++ url_instax/routers/api/v1/screenshot.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter, Response
+from fastapi import APIRouter, Query, Response
 from pydantic import BaseModel
+
+from url_instax.log import logger
+from url_instax.main import take_screenshot
 
 router = APIRouter(
     tags=["Component"],
@@ -7,25 +10,68 @@ router = APIRouter(
 )
 
 
-class ScreenshotResponse(Response):
-    media_type = "image/png"
-
-    def render(self, content: bytes) -> bytes:
-        self.headers["Content-Disposition"] = 'inline; filename="screenshot.png"'
-        return super().render(content)
+def response_image(
+    image: bytes,
+) -> Response:
+    return Response(
+        content=image,
+        media_type="image/png",
+        headers={
+            "Content-Disposition": "inline; filename=screenshot.png",
+        },
+    )
 
 
 @router.get("/screenshot")
-async def screenshot_get() -> ScreenshotResponse:
-    return
+async def screenshot_get(
+    url: str = Query(..., description="URL to take a screenshot of"),
+    is_mobile: bool = Query(
+        False,
+        description="Whether to use mobile emulation",
+    ),
+    timeout: float = Query(
+        30,
+        description="Timeout in seconds",
+    ),
+    full_page: bool = Query(
+        True,
+        description="Whether to take a full page screenshot",
+    ),
+) -> Response:
+    logger.debug(f"Taking screenshot of {url}")
+    return response_image(
+        await take_screenshot(
+            url=url,
+            is_mobile=is_mobile,
+            timeout=timeout,
+            full_page=full_page,
+        )
+    )
 
 
 class ScreenshotParams(BaseModel):
     url: str
+    user_agent: str | None = None
+    extra_http_headers: dict[str, str] | None = None
+    is_mobile: bool = False
+    timeout: float = 30
+    full_page: bool = True
+    wait_for: int | None = None
 
 
 @router.post("/screenshot")
 async def screenshot_post(
     params: ScreenshotParams,
-) -> ScreenshotResponse:
-    return
+) -> bytes:
+    logger.debug(f"Taking screenshot of {params.url} with params: {params.model_dump()}")
+    return response_image(
+        await take_screenshot(
+            url=params.url,
+            user_agent=params.user_agent,
+            extra_http_headers=params.extra_http_headers,
+            is_mobile=params.is_mobile,
+            timeout=params.timeout,
+            full_page=params.full_page,
+            wait_for=params.wait_for,
+        )
+    )
